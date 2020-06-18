603d60fa9565724584a9924cf916492b19582817
Artist0618 <991087176@qq.com>
2020-06-18 20:02:07 +0800

    No remarks

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by: PhpStorm
+ * User: Artist
+ * Date: 2020/6/18
+ * Time: 7:44 上午
+ */
+
+namespace app\api\controller\v1;
+
+
+class Address
+{
+    public function createOrUpdateAddress(){
+
+    }
+}
---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by: PhpStorm
+ * User: Artist
+ * Date: 2020/6/18
+ * Time: 7:46 上午
+ */
+
+namespace app\api\validate;
+
+
+class AddressNew extends BaseValidate
+{
+    protected $rule = [
+        'name' => 'require|isNotEmpty',
+        'mobile' => 'require|isMobile',
+        'provice' => 'require|isNotEmpty',
+        'city' => 'require|isNotEmpty',
+        'country' => 'require|isNotEmpty',
+        'detail' => 'require|isNotEmpty',
+    ];
+}
